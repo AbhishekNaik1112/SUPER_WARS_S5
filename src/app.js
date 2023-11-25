1ecf35a802e90e17e55795403cc63cfaed535149
AbhishekNaik1112 <abhisek.naik@kalvium.community>
2023-11-25 09:32:42 +0530

    js for calculate score

--- src/app.js
+++ src/app.js
@@ -26,7 +26,7 @@ class Player {
   constructor(id, name, type) {
     this.id = id;
     this.name = name;
-    this.image = "images/super-" + (id + 1) + ".png";
+    this.image = `images/super-${id + 1}.png`;
     this.strength = this.getRandomStrength();
     this.type = type;
     this.selected = false;
@@ -34,21 +34,19 @@ class Player {
   }
 
   // Get random strength
-  getRandomStrength = () => {
-    return Math.ceil(Math.random() * 100);
-  };
+  getRandomStrength = () => Math.ceil(Math.random() * 100);
 
   // Create a player for displaying
   view = () => {
-    let player = document.createElement("div");
+    const player = document.createElement("div");
     player.classList.add("player");
     player.setAttribute("data-id", this.id);
-    if (this.selected == true) player.classList.add("selected");
-    let image = document.createElement("img");
+    if (this.selected === true) player.classList.add("selected");
+    const image = document.createElement("img");
     image.setAttribute("src", this.image);
-    let name = document.createElement("div");
+    const name = document.createElement("div");
     name.textContent = this.name;
-    let strength = document.createElement("div");
+    const strength = document.createElement("div");
     strength.textContent = this.strength;
     strength.className = "strength";
     player.append(image, name, strength);
@@ -60,10 +58,11 @@ class Player {
 class Superwar {
   constructor(players) {
     this.players = players.map((player, i) => {
-      let type = i % 2 == 0 ? "hero" : "villain";
+      const type = i % 2 === 0 ? "hero" : "villain";
       return new Player(i, player, type);
     });
-    this.score = [0, 0];
+    this.score = { hero: 0, villain: 0 };
+    this.strength = { hero: 0, villain: 0 };
     Array.from(document.getElementsByClassName("team")).forEach((elem) =>
       elem.addEventListener("click", (e) => {
         this.handleSelection(e.target);
@@ -86,7 +85,7 @@ class Superwar {
 
   // Build players fragment
   buildPlayers = (type) => {
-    let fragment = document.createDocumentFragment();
+    const fragment = document.createDocumentFragment();
     this.filterPlayers(type).forEach((player) =>
       fragment.append(player.view())
     );
@@ -94,19 +93,18 @@ class Superwar {
   };
 
   // Filter Players based on type
-  filterPlayers = (type) => {
-    return this.players.filter((player) => player.type == type);
-  };
+  filterPlayers = (type) =>
+    this.players.filter((player) => player.type === type);
 
   // Handle player clicks
   handleSelection = (target) => {
     if (!target.classList.contains("player")) target = target.parentNode;
     if (!target.hasAttribute("data-id")) return;
 
-    let selectedId = target.getAttribute("data-id");
-    let selectedPlayer = this.players[selectedId];
+    const selectedId = target.getAttribute("data-id");
+    const selectedPlayer = this.players[selectedId];
     this.players
-      .filter((player) => player.type == selectedPlayer.type)
+      .filter((player) => player.type === selectedPlayer.type)
       .forEach((player) => (player.selected = false));
     selectedPlayer.selected = true;
 
@@ -115,67 +113,46 @@ class Superwar {
   };
 
   // Progression 1: Check for fight
-  isFight = (strength) => {
-    return strength > 0 ? "clash" : "peace";
-  };
+  isFight = (strength) => (strength > 0 ? "clash" : "peace");
 
   // Fight
   fight = () => {
-    // Filtered the selected players and calculate score
-    let score = this.calculateScore();
-    console.log(score);
-    let scoreCard = document.getElementById("score");
-    scoreCard.innerHTML = score[0] + " - " + score[1];
-    // Should return HTML element with score
-    // Type your code here
+    const score = this.calculateScore();
+    const scoreCard = document.getElementById("score");
+    scoreCard.innerHTML = `${score.hero} - ${score.villain}`;
 
     if (this.checkWin() !== "endure")
       setTimeout(() => this.announceWinner(score), 100);
-    return scoreCard;
   };
 
   // Progression 2: Calculate score
   calculateScore = () => {
-    let result = this.checkWin();
-    console.log(result);
-    if (result == "hero") this.score[0] = 1;
-    else if (result == "villain") this.score[1] = 1;
+    const result = this.checkWin();
+    if (result === "hero") return { hero: 1, villain: 0 };
+    else if (result === "villain") return { hero: 0, villain: 1 };
     return this.score;
-
-    // Calculate and return the total score of teams
-    // Type your code here
   };
 
   // Progression 3: Check whether there is a win
   checkWin = () => {
-    let heroStrength = this.totalStrength("hero");
-    let villainStrength = this.totalStrength("villain");
+    const heroStrength = this.totalStrength("hero");
+    const villainStrength = this.totalStrength("villain");
 
     if (heroStrength > villainStrength) return "hero";
     else if (heroStrength === villainStrength) return "endure";
     else return "villain";
-    // Find the winner if exists return type hero or villain
-    // If winner dosen't exists then return endure
-    // Type your code here
   };
 
   // Progression 4: Find total strength of a team
-
-  totalStrength = (type) => {
-    let strength = 0;
-    this.players.map((player) => {
-      player.type == type ? (strength += player.strength) : null;
-    });
-    // Calculate and return the total strength of the team
-    // Type your code here
-
-    return strength;
-  };
+  totalStrength = (type) =>
+    this.players
+      .filter((player) => player.type === type)
+      .reduce((total, player) => total + player.strength, 0);
 
   // Announce the winner
   announceWinner = (score) => {
-    if (score["hero"] == score["villain"]) alert("Its a draw!");
-    else if (score["hero"] > score["villain"]) alert("Heroes Win!");
+    if (score.hero === score.villain) alert("It's a draw!");
+    else if (score.hero > score.villain) alert("Heroes Win!");
     else alert("Villains Win!");
     location.reload();
   };
